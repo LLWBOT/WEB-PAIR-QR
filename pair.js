c45abe982162c3c16d17a1c0feb6cb58d28d9006
LLWBOT <gamingchamiya344@gmail.com>
2025-03-24 17:31:36 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -17,7 +17,6 @@ const MESSAGE = process.env.MESSAGE || `
 
 *LLW MD V1 2025* 🗿
 `;
-await conn.sendMessage(from,{image:{url:"https://files.catbox.moe/egqzxo.jpg"},caption:up},{quoted:mek})
 
 const { upload } = require('./mega');
 const {
